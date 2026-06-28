7529602cbf5eea6559c45283ae3ad9a9406384e7
lleonards <lleonards.777@gmail.com>
2026-06-28 15:40:47 -0300

    Delete Dimensy_Backend directory

--- Dimensy_Backend/src/lib/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import ws from 'ws';
-
-export const supabaseAdmin = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE, {
-  auth: { autoRefreshToken: false, persistSession: false },
-  realtime: { transport: ws },
-});
